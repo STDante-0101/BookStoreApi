30ca191def2c61c482dce772c270d601a4cc89a2
STDante-0101 <marcolinodiego.stn@gmail.com>
2024-03-08 19:19:01 -0300

    Exclusão da classes que não seram utilizadas, e criação do .gitignore

--- BookStoreApi/obj/Release/net8.0/BookStoreApi.AssemblyInfo.cs
+++ BookStoreApi/obj/Release/net8.0/BookStoreApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ee826201f37cc038cfecc93ee6ca2bbf290c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApi")]
